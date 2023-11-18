ab00dcb3ae04860afe783278be661beba82ce664
nokekip <kipnoke@gmail.com>
2023-11-18 23:50:09 +0300

    added reusable files for footer, navbar and idebar

---
+++ app/includes/viewsFooter.php
@@ -0,0 +1,19 @@
+<footer class="footer mt-auto">
+    <div class="container-fluid">
+        <div class="row text-muted">
+            <div class="col-6 text-center">
+                <p class="mb-0"></p><strong>© 2023 noke</strong></p>
+            </div>
+            <div class="col-6 text-end">
+                <ul class="list-inline">
+                    <li class="list-inline-item">
+                        <a href="../index.php" class="text-muted">Home</a>
+                    </li>
+                    <li class="list-inline-item">
+                        <a href="#" class="text-muted">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
