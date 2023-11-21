d6120cd35c0071e75475adbc0a4da2ca2f6e4be6
nokekip <kipnoke@gmail.com>
2023-11-21 22:59:34 +0300

    implement login validation and login user

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -11,8 +11,11 @@ $phone = '';
 $password = '';
 $confPassword = '';
 
+$table = 'Users';
+
 // func for session login
-function loginUser($user) {
+function loginUser($user)
+{
     $_SESSION['UserID'] = $user['UserID'];
     $_SESSION['username'] = $user['username'];
     $_SESSION['message'] = 'You are are now logged in';
@@ -28,8 +31,8 @@ if (isset($_POST['register-btn'])) {
     if (count($errors) === 0) {
         unset($_POST['register-btn'], $_POST['confpassword']);
         $_POST['password'] = password_hash($_POST['password'], PASSWORD_DEFAULT);
-        $user_id = create('Users', $_POST);
-        $user = selectOne('Users', ['UserID' => $user_id]);
+        $user_id = create($table, $_POST);
+        $user = selectOne($table, ['UserID' => $user_id]);
         // login user
         loginUser($user);
     } else {
@@ -39,5 +42,23 @@ if (isset($_POST['register-btn'])) {
         $phone = $_POST['phone'];
         $password = $_POST['password'];
         $confPassword = $_POST['confpassword'];
+    }
+}
+
+// login validation
+if (isset($_POST['login-btn'])) {
+    $errors = validateLogin($_POST);
+
+    // check if no errors
+    if (count($errors) === 0) {
+        $user = selectOne($table, ['username' => $_POST['username']]);
+        if ($user && password_verify($_POST['password'], $user['password'])) {
+            loginUser($user);
+        } else {
+            array_push($errors, 'Invalid username or password');
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+        }
     }
 }
