306aabf17ac4d095ee3504dfb8c9af7102fda530
nokekip <kipnoke@gmail.com>
2023-11-21 18:57:45 +0300

    validate user registration input before submiting to database

---
+++ app/helpers/validateUser.php
@@ -0,0 +1,30 @@
+<?php
+
+// validate register form to make sure required fields are filled
+function validateUser($user) {
+    $errors = array();
+
+    if(empty($user['username'])) {
+        array_push($errors, 'Username is required');
+    }
+    if(empty($user['name'])) {
+        array_push($errors, 'Your name is required');
+    }
+    if(empty($user['email'])) {
+        array_push($errors, 'Email is required');
+    }
+    if(empty($user['phone'])) {
+        array_push($errors, 'Phone Number is required');
+    }
+    if(empty($user['password'])) {
+        array_push($errors, 'Password is required');
+    }
+    if(empty($user['confpassword'])) {
+        array_push($errors, 'Confirm Password is required');
+    }
+    if($user['password'] !== $user['confpassword']) {
+        array_push($errors, 'Password does not match');
+    }
+
+    return $errors;
+}
