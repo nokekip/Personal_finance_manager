aa414154a25b1ff72da37a6405f75bfe75544cb8
nokekip <kipnoke@gmail.com>
2023-11-25 15:50:27 +0300

    validate categoty form

---
+++ app/helpers/validateCategory.php
@@ -0,0 +1,18 @@
+<?php
+
+function validateCategory($category)
+{
+    $errors = array();
+
+    if (empty($category['username'])) {
+        array_push($errors, 'Category name is required');
+    }
+  
+
+    $existingCategory = selectOne('Category', ['categoryName' => $category['categoryName']]);
+    if ($existingCategory) {
+        array_push($errors, 'Category already exist!');
+    }
+
+    return $errors;
+}
